247783750d62aeb0f52e284b6a44c6a9a7220860
Sam Colby <samcolby@users.noreply.github.com>
2019-06-06 12:32:19 +0100

    Fix appcenter.ms post-clone script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-if [ -e $APPCENTER_SOURCE_DIRECTORY/index.js ]; then
-  echo "This is the correct directory"
-else
-  echo "Oh where am I then?"
-fi
-
 # Generate env.json using App Center Environment variables
 echo "{\"uri\":\"$DEV_ENV_GRAPHQL_URI\",\"hasura_access_key\":\"$DEV_ENV_ACCESS_SECRET\"}" > $APPCENTER_SOURCE_DIRECTORY/env.json
-
-cat $APPCENTER_SOURCE_DIRECTORY/env.json
