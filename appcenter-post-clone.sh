8c6c1e744175ca0730285abd241ccc5031bb5680
Sam Colby <samcolby@users.noreply.github.com>
2019-06-06 11:55:48 +0100

    Attempt 5 to fix "UnicodeEscapeSequence error"

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -e $APPCENTER_SOURCE_DIRECTORY/babel.config.js ]; then
+if [ -e $APPCENTER_SOURCE_DIRECTORY/index.js ]; then
   echo "This is the correct directory"
 else
   echo "Oh where am I then?"
@@ -9,3 +9,5 @@ fi
 # Generate env.json using App Center Environment variables
 echo "$APPCENTER_SOURCE_DIRECTORY/env.json"
 echo $DEV_ENV_JSON > $APPCENTER_SOURCE_DIRECTORY/env.json
+
+cat $APPCENTER_SOURCE_DIRECTORY/env.json
