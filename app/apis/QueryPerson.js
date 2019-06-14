757df6c70b331d78cc79da72f171e7e564bd9208
Sam Colby <samcolby@users.noreply.github.com>
2019-06-14 13:46:27 +0100

    Tidied up comments

--- app/apis/QueryPerson.js
+++ app/apis/QueryPerson.js
@@ -7,9 +7,10 @@ import { Query } from "react-apollo";
 import { QUERY_PERSON } from "../GraphQLQueries";
 
 /**
- * Apollo Query to return a list of people to display on a map
+ * Apollo Query to return the data for a person based on their id
  * @param {Object} props - Standard react prop
  * @param {function} children - Standard react prop, doesn't need to be included
+ * @param {string} id - the id of the person to retrieve
  */
 function QueryPerson({ children, id }) {
   return (
