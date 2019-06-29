2f7b2919e25ae50a6a8949674e12868dbe17afdc
Sam Colby <samcolby@users.noreply.github.com>
2019-06-29 14:58:25 +0100

    Convert to function component

--- app/components/FormTextField.js
+++ app/components/FormTextField.js
@@ -1,53 +1,45 @@
-import React, { PureComponent } from "react";
+import React, { memo } from "react";
 import PropTypes from "prop-types";
 
 import { View } from "react-native";
 import { Input } from "react-native-elements";
 
+const getErrorMessage = (fieldName, arrErrors, touched) => {
+  if (arrErrors[fieldName] && touched[fieldName]) {
+    return arrErrors[fieldName];
+  } else {
+    return null;
+  }
+};
+
 /**
  * React Pure Component to display
  * a label, text field and error message
- * @class FormTextField
- * @extends {PureComponent}
  */
-class FormTextField extends PureComponent {
-  static propTypes = {
-    fieldName: PropTypes.string.isRequired,
-    formikProps: PropTypes.object.isRequired,
-    label: PropTypes.string.isRequired
-  };
+const FormTextField = props => {
+  const { fieldName, formikProps, label, ...passthruProps } = props;
+  const { errors, handleBlur, handleChange, touched, values } = formikProps;
 
-  constructor(props) {
-    super(props);
-  }
+  const errorMessage = getErrorMessage(fieldName, errors, touched);
 
-  _getErrorMessage = (fieldName, arrErrors, touched) => {
-    if (arrErrors[fieldName] && touched[fieldName]) {
-      return arrErrors[fieldName];
-    } else {
-      return null;
-    }
-  };
+  return (
+    <View>
+      <Input
+        errorMessage={errorMessage}
+        label={label}
+        onChangeText={handleChange(fieldName)}
+        onBlur={handleBlur(fieldName)}
+        value={values[fieldName]}
+        {...passthruProps}
+      />
+    </View>
+  );
+};
 
-  render() {
-    const { fieldName, formikProps, label, ...passthruProps } = this.props;
-    const { errors, handleBlur, handleChange, touched, values } = formikProps;
+FormTextField.propTypes = {
+  fieldName: PropTypes.string.isRequired,
+  formikProps: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired
+};
 
-    const errorMessage = this._getErrorMessage(fieldName, errors, touched);
-
-    return (
-      <View>
-        <Input
-          errorMessage={errorMessage}
-          label={label}
-          onChangeText={handleChange(fieldName)}
-          onBlur={handleBlur(fieldName)}
-          value={values[fieldName]}
-          {...passthruProps}
-        />
-      </View>
-    );
-  }
-}
-
-export default FormTextField;
+export default memo(FormTextField);
