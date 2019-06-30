6d09f71e61079f4a43b0d8aa8dd741bf7430de34
Sam Colby <samcolby@users.noreply.github.com>
2019-06-30 16:50:05 +0100

    Updated for new env variable

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Generate env.json using App Center Environment variables
-echo "{\"api_uri\":\"$DEV_API_URI\",\"api_access_key_header\":\"$DEV_API_ACCESS_KEY_HEADER\",\"api_access_key\":\"$DEV_API_ACCESS_KEY\"}" > $APPCENTER_SOURCE_DIRECTORY/env.json
+echo "{\"api_uri\":\"$DEV_API_URI\",\"api_access_key_header\":\"$DEV_API_ACCESS_KEY_HEADER\",\"api_access_key\":\"$DEV_API_ACCESS_KEY\",\"static_maps_key\":\"$DEV_STATIC_MAPS_KEY\"}" > $APPCENTER_SOURCE_DIRECTORY/env.json
