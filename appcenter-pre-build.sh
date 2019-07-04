84284e0ec75387b0e9ad90b7b759120ade62f67d
Sam Colby <samcolby@users.noreply.github.com>
2019-07-04 21:47:15 +0100

    fix: CI again 6

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# echo "GoogleMapsiOSKey=$GoogleMapsiOSKey" > $APPCENTER_SOURCE_DIRECTORY/ios/.env
-
 # If CI is for an iOS build then sort out cocoapods-keys
 # by running pod install to generate the keys
-if [ $OS_TYPE != "iOS" ];
+if [ $OS_TYPE = "iOS" ];
 then
   echo "Running iOS pre-build to generate cocoapods-keys"
   gem install cocoapods-keys
