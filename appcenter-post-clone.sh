0331f00115e8a216c1d57b4947cfe9096c9ed937
Sam Colby <samcolby@users.noreply.github.com>
2019-07-04 17:30:59 +0100

    style: tidy up json string

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 # Generate env.json using App Center Environment variables
-echo "{\"api_uri\":\"$DEV_API_URI\",\"api_access_key_header\":\"$DEV_API_ACCESS_KEY_HEADER\",\"api_access_key\":\"$DEV_API_ACCESS_KEY\",\"static_maps_key\":\"$DEV_STATIC_MAPS_KEY\",\"android_maps_key\":\"$DEV_ANDROID_MAPS_KEY\"}" > $APPCENTER_SOURCE_DIRECTORY/env.json
+
+json="{\"api_uri\":\"$DEV_API_URI\","
+json="$json\"api_access_key_header\":\"$DEV_API_ACCESS_KEY_HEADER\","
+json="$json\"api_access_key\":\"$DEV_API_ACCESS_KEY\","
+json="$json\"static_maps_key\":\"$DEV_STATIC_MAPS_KEY\","
+json="$json\"android_maps_key\":\"$DEV_ANDROID_MAPS_KEY\"}"
+
+echo $json > $APPCENTER_SOURCE_DIRECTORY/env.json
+
