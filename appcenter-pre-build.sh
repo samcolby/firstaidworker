d90ad8f303421fceecb3e867c69f139bb9c932f8
Sam Colby <samcolby@users.noreply.github.com>
2019-07-04 21:32:27 +0100

    fix: CI again 4

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,8 +2,10 @@
 
 # echo "GoogleMapsiOSKey=$GoogleMapsiOSKey" > $APPCENTER_SOURCE_DIRECTORY/ios/.env
 
-gem install cocoapods-keys
-
-cd ios && pod keys generate
-
-echo "The react native package is $APPCENTER_REACTNATIVE_PACKAGE"
+# If CI is for an iOS build then sort out cocoapods-keys
+# by running pod install to generate the keys
+if [ "$OS_TYPE" != "iOS" ];
+then
+  gem install cocoapods-keys
+  cd ios && pod install
+fi
