a060fe80b65ae968a3d4110dd1253d8b751f1e04
Sam Colby <samcolby@users.noreply.github.com>
2019-07-04 21:40:25 +0100

    fix: CI again 5

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,8 +4,11 @@
 
 # If CI is for an iOS build then sort out cocoapods-keys
 # by running pod install to generate the keys
-if [ "$OS_TYPE" != "iOS" ];
+if [ $OS_TYPE != "iOS" ];
 then
+  echo "Running iOS pre-build to generate cocoapods-keys"
   gem install cocoapods-keys
   cd ios && pod install
+else
+  echo "Running Android pre-build script"
 fi
